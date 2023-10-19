69702f4675dd778880632377ed4fa96a844cc625
wwzh2626 <2801731537@qq.com>
2023-10-19 09:10:57 +0800

    Add Git LFS tracking for large file

---
+++ VGG/predict.py
@@ -0,0 +1,40 @@
+import torch
+import os
+import json
+from PIL import Image
+from torchvision import transforms
+from module import vgg
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    data_transform = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    # load image
+    data_root = os.path.dirname(os.getcwd())
+    image_path = os.path.join(data_root, "dataset/predict/1.jpeg")
+    assert os.path.exists(image_path), f"image_path: {image_path} is not exist ."
+    img = Image.open(image_path)
+    plt.imshow(img)
+    # transforms image into [NxCxHxW]
+    img = data_transform(img)
+    img = torch.unsqueeze(img, dim=0)
+
+    # read class_indict
+    json_path = './class_indices.json'
+    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
+
+    with open(json_path, "r") as f:
+        class_indict = json.load(f)
+
+
+
+if __name__ == '__main__':
+    main()
+    print("fff")
